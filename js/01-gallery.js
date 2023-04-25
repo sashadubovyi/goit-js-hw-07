49500da32c5b23c58f68586010da8fe4c3d82e03
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-04-25 23:23:15 +0300

    homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const ulGallery = document.querySelector(".gallery");
+const addGallaryMarkup = createGallery(galleryItems);
+
+ulGallery.innerHTML = addGallaryMarkup;
+ulGallery.addEventListener("click", onImageClick);
+
+function createGallery(images) {
+  return images
+    .map(
+      (image) =>
+        `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+          />
+        </a>
+      </li>
+    `
+    )
+    .join("");
+}
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
+  instance.show();
+
+  ulGallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const ulGallery = document.querySelector(".gallery");
+const addGallary = createGallery(galleryItems);
+
+ulGallery.innerHTML = addGallary;
+
+function createGallery(images) {
+  return images
+    .map(
+      (image) =>
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img class="gallery__image" src="${image.preview}" alt="${image.description}" title="${image.description}" />
+            </a>
+        </li>
+    `
+    )
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
